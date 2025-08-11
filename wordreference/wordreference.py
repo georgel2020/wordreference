226686a8319b53026b18dfd4b44646b2058387c9
georgel2020 <86717650+georgel2020@users.noreply.github.com>
2025-08-11 17:11:05 +0800

    Add WordReference scraper
    - Implement `get_dictionary` function to scrape word definitions from WordReference
    - Add dependencies on `bs4` and `requests` in `pyproject.toml`
    - Include testing usage in `main.py`

---
+++ wordreference/wordreference.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_dictionary(word: str) -> tuple:
+    url = f'https://www.wordreference.com/enzh/{word}'
+    response = requests.get(url)
+    if response.status_code != 200:
+        return '', ''
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    article_wrd = soup.find('div', id='articleWRD')
+    table_rows = article_wrd.find_all('tr', class_=['odd', 'even', 'wrtopsection'])
+
+    meanings = []
+    sections = []
+    current_section = -1
+    current_index = 0
+    last_class = 'odd'
+    for row in table_rows:
+        if 'wrtopsection' in row.get('class'):
+            sections.append(row.get_text())
+            current_section += 1
+            meanings.append([])
+            current_index = 0
+            last_class = 'odd'
+            continue
+        if last_class not in row.get('class'):
+            last_class = row.get('class')[0]
+            current_index += 1
+
+        if len(row.find_all('td')) >= 3:
+            columns = row.find_all('td')
+            if columns[0].get('class') == ['FrWrd']:
+                meanings[current_section].append({})
+                meanings[current_section][current_index]['meaning'] = columns[0].find('strong').get_text()
+                if columns[0].find('em', class_='POS2'):
+                    meanings[current_section][current_index]['part_of_speech'] = columns[0].find('em', class_='POS2').get_text()
+                meanings[current_section][current_index]['synonym'] = columns[1].get_text()
+            if columns[2].get('class') == ['ToWrd']:
+                zhgroups = columns[2].find_all('span', class_='zhgroup')
+                simplified_zhgroups = [zhgroup for zhgroup in zhgroups if zhgroup.find('span', class_='simplified')]
+                for simplified_zhgroup in simplified_zhgroups:
+                    if simplified_zhgroup.find('span', class_='simplified'):
+                        simplified_zhgroup.find('span', class_='simplified').extract()
+                    if simplified_zhgroup.find('span', class_='pinyintxt'):
+                        simplified_zhgroup.find('span', class_='pinyintxt').extract()
+                simplified_translation = '，'.join([simplified_zhgroup.get_text().strip() for simplified_zhgroup in simplified_zhgroups])
+                if 'simplified_translations' in meanings[current_section][current_index].keys():
+                    meanings[current_section][current_index]['simplified_translation'] += '，' + simplified_translation
+                else:
+                    meanings[current_section][current_index]['simplified_translation'] = simplified_translation
+
+        if len(row.find_all('td')) == 2:
+            sentence_td = row.find('td', colspan='2')
+            if sentence_td:
+                if sentence_td.get('class') == ['FrEx']:
+                    meanings[current_section][current_index]['example'] = sentence_td.get_text()
+                if sentence_td.get('class') == ['ToEx']:
+                    meanings[current_section][current_index]['example_translation'] = sentence_td.get_text()
+
+    return sections, meanings
