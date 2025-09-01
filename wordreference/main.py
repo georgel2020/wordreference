76c8fcf1a9a440bb0fdfefbc0a955ab1495de2e4
georgel2020 <lingeorge2020@outlook.com>
2025-09-01 17:48:28 +0800

    Add file output support to CLI
    - Add `-f`/`--file` argument to specify output file
    - Redirect output to specified file if `-f` is used
    - Implement `NoColors` class to handle colorless output for files
    - Fallback to standard output and colored output when no file is specified

--- wordreference/main.py
+++ wordreference/main.py
@@ -1,3 +1,5 @@
+import sys
+
 from wordreference.wordreference import *
 from wordreference.cli import *
 
@@ -5,22 +7,29 @@ def main():
     args = get_args()
     brief_mode = args.brief
 
+    if args.filename:
+        output_stream = open(args.filename, 'w', encoding='utf-8')
+        colors = NoColors()
+    else:
+        colors = Colors()
+        output_stream = sys.stdout
+
     for word in args.words:
         dictionary = get_dictionary(word)
 
         error = dictionary.get('error')
         if error:
-            print(f'{Colors.BRIGHT_RED}在 WordReference 英-汉词典中，没有发现 \'{word}\' 的翻译{Colors.RESET}')
+            print(f'{colors.BRIGHT_RED}在 WordReference 英-汉词典中，没有发现 \'{word}\' 的翻译{colors.RESET}', file=output_stream)
             return
 
         uk_pronunciation = dictionary['pronunciation'].get('uk', '')
         us_pronunciation = dictionary['pronunciation'].get('us', '')
         sections = dictionary.get('sections', [])
         meanings = dictionary.get('meanings', [])
-        print(f'{Colors.BRIGHT_YELLOW}{word}{Colors.RESET}{Colors.YELLOW}{' UK ' + uk_pronunciation if uk_pronunciation else ''}{' US ' + us_pronunciation if us_pronunciation else ''}{Colors.RESET}')
+        print(f'{colors.BRIGHT_YELLOW}{word}{colors.RESET}{colors.YELLOW}{' UK ' + uk_pronunciation if uk_pronunciation else ''}{' US ' + us_pronunciation if us_pronunciation else ''}{colors.RESET}', file=output_stream)
 
         for index in range(len(sections)):
-            print(f'{Colors.BRIGHT_RED}{sections[index]}{Colors.RESET}')
+            print(f'{colors.BRIGHT_RED}{sections[index]}{colors.RESET}', file=output_stream)
             for meaning in meanings[index]:
                 source = meaning.get('source', '')
                 part_of_speech = meaning.get('part_of_speech', '')
@@ -28,11 +37,13 @@ def main():
                 translation = meaning.get('translation', '')
                 example = meaning.get('example', '')
                 example_translation = meaning.get('example_translation', '')
-                print(f'{Colors.BRIGHT_CYAN}{source}{Colors.RESET} {Colors.BRIGHT_BLUE}{part_of_speech}{Colors.RESET} {synonym} {translation}')
+                print(f'{colors.BRIGHT_CYAN}{source}{colors.RESET} {colors.BRIGHT_BLUE}{part_of_speech}{colors.RESET} {synonym} {translation}', file=output_stream)
                 if example and not brief_mode:
-                    print(f'{Colors.DARK_GRAY}{example}{Colors.RESET}')
+                    print(f'{colors.DARK_GRAY}{example}{colors.RESET}', file=output_stream)
                 if example_translation and not brief_mode:
-                    print(f'{Colors.DARK_GRAY}{example_translation}{Colors.RESET}')
+                    print(f'{colors.DARK_GRAY}{example_translation}{colors.RESET}', file=output_stream)
+
+        print('', file=output_stream)
 
 if __name__ == "__main__":
     main()
